de8c7b09990c3bf3b8a2854c7403ce8d9e0e0b42
David Herman <david.herman@gmail.com>
2018-08-21 07:13:23 -0700

    Initial UA parsing.

---
+++ functions/ua.js
@@ -0,0 +1,11 @@
+var ua = require('useragent');
+
+exports.handler = function(event, context, callback) {
+    var agent = ua.parse(event.headers['user-agent'];
+
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(agent.os)
+    });
+};
+
