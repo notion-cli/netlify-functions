94e0d938a1137237af23be5960a04f15e5ae13cb
David Herman <david.herman@gmail.com>
2018-08-22 05:09:12 -0700

    add test route `/ua` for testing UA sniffing

--- func/get.js
+++ func/get.js
@@ -48,7 +48,7 @@ exports.handler = function(event, context, callback) {
   var ua = event.headers['user-agent'];
   var os = family(ua);
 
-  if (os === null) {
+  if (os === null || event.queryStringParameters['route'] === '/ua') {
     return callback(null, {
       statusCode: 400,
       body: "Unrecognized operating system: " + ua + "\n\n" + JSON.stringify(event.headers, null, 2) + "\n"
