24f5b76dcbdd26b7c0ac0037d9f1bd0b5d1dae87
David Herman <david.herman@gmail.com>
2018-08-21 22:30:09 -0700

    let's try using platform.js instead

--- func/ua.js
+++ func/ua.js
@@ -1,11 +1,50 @@
-var parser = require('ua-parser-js');
+var platform = require('platform');
+
+var linuxes = [
+  'CentOS',
+  'Gentoo',
+  'Kubuntu',
+  'Linux Mint',
+  'Linux',
+  'Xubuntu,
+  'Ubuntu',
+  'Debian',
+  'Fedora',
+  'Red Hat',
+  'SuSE'
+];
+
+var macs = [
+  'Mac OS X',
+  'OS X',
+  'Mac',
+  'Macintosh'
+];
+
+function family(ua) {
+  var family = platform.parse(ua).os.family;
+
+  if (/^Windows/.test(family) && family !== 'Windows Phone') {
+    return 'windows';
+  }
+
+  if (linuxes.indexOf(family) >= 0) {
+    return 'linux';
+  }
+
+  if (macs.indexOf(family) >= 0) {
+    return 'mac';
+  }
+
+  return null;
+}
 
 exports.handler = function(event, context, callback) {
     var ua = parser(event.headers['user-agent']);
 
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(ua.os)
+        body: family(
     });
 };
 
